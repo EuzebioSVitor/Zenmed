7f5fb8159429365e1b5f923d237e41b0fdb23c79
Arthur Leonardo <62223645+Arthur-Leonardo@users.noreply.github.com>
2024-09-26 17:06:16 -0300

    Update
    melhoria nos popups e 1 artigo de teste, com um arquivo como base de dados toda esquisita mas funciona

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ const cards = document.querySelectorAll('.card');
 const popup = document.querySelector('.popup');
 const popupContent = document.querySelector('.popup_content');
 
-const cardContent = {
-  "Título 1": "Lorem Ipsum dolor et aaaaa",
-  "Título 2": "Muito conteúdo zika.",
-  "Título 3": "artigo incrível cheio de coisas maravilhosas e vislumbrantes, uau, que mágico",
-  "Título 4": "As vezes eu me pergunto se a melhor ideia é realmente melhor.",
-};
-
 cards.forEach(card => {
   card.addEventListener('click', () => {
     const cardTitle = card.querySelector('.card_title').textContent;
@@ -18,7 +11,7 @@ cards.forEach(card => {
       <h2>${cardTitle}</h2>
       <p>${content}</p>
       <div class="center">
-      <button onclick="fecharpopup()" class="popup_close">Fechar</button>
+      <button onclick="fecharpopup()" class="popup_close">X</button>
       </div>
     `;
     popup.style.opacity = 1;
